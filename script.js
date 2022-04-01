86f9e5ae14e070f5011bbb730ac81829dd61d428
dokebu <gamerdokebu@gmail.com>
2022-04-01 01:04:00 -0300

    2 files modified

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ appForm.onsubmit = dolarprareal;
 function dolarprareal(e){
 	e.preventDefault();
 
-	var input_taxa = document.getElementById("input_taxa").value;
+	//var input_taxa = document.getElementById("input_taxa").value;
   var input_dollar = document.getElementById("input_dollar").value;
-  var input_real = document.getElementById("input_real").value;
+  //var input_real = document.getElementById("input_real").value;
 	var result = document.getElementById("result");
   var resultdollar = document.getElementById("resultdollar");
   var resultreal = document.getElementById("resultreal");
@@ -16,21 +16,21 @@ function dolarprareal(e){
 	var result_dollar = document.getElementById("resultdollar");
 
 	var valor_dollar = parseFloat(input_dollar);
-  var valor_taxa = parseFloat(input_taxa);
-  var valor_real = parseFloat(input_real);
+  //var valor_taxa = parseFloat(input_taxa);
+  //var valor_real = parseFloat(input_real);
 
 	result.innerHTML = valor_dollar;
-  resultdollar.innerHTML = valor_dollar;
-  resultreal.innerHTML = valor_real;
+  //resultdollar.innerHTML = valor_dollar;
+  //resultreal.innerHTML = valor_real;
 
 	var total = calculo(valor_dollar);
 
-	result_dollar.innerHTML = "Reusltado da conversão: " + total;
+	result_dollar.innerHTML = "Reusltado da conversão: R$ " + total;
 }
 
-function calculo(valor_dollar){
+function calculo(valor_dollar, resultado, valor_taxa){
 	var total = valor_dollar;
 	var quarters = 0;
-  //var resultado = valor_taxa * valor_dollar;
+  var resultado = valor_taxa * valor_dollar;
 	return [total];
 }
